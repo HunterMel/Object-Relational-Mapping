ee3665a39fe54c7cd22bfe4eb72baff0b87678c4
Hunter Johnson <hunterjohnsoninfo@gmail.com>
2021-07-19 13:31:31 -0500

    added categories/endpoints

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,19 +5,42 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [Product]
+  })
+  .then((results) => {
+    res.json(results);
+  })
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((results) => {
+    res.json(results);
+  })
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(newCategory => {
+    res.json(newCategory)
+  })
 });
 
 router.put('/:id', (req, res) => {
+
+  // look for the specific category that had this "id",
+  // update this post with the req.body
+
   // update a category by its `id` value
 });
 
